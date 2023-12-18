6ed491a363c6859158d152edf3be403e8fe75e1d
Moenmyna <153787659+Moenmyna@users.noreply.github.com>
2023-12-18 14:21:55 -0500

    Initial commit

---
+++ dice20Roll.py
@@ -0,0 +1,11 @@
+import random
+
+dice20 = (random.randrange(1, 21))
+roll = "You rolled a {}!"
+
+print(roll.format(dice20))
+
+if dice20 == 20:
+    print("CRITICAL SUCCESS!")
+elif dice20 == 1:
+    print("CRITICAL FAIL!")
